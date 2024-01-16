8813db9f21d327f3bfd6a2f92ddf6b0ffb842031
SergeTopLigin <topligahighlights@gmail.com>
2024-01-16 23:00:41 +0300

    templates adding

--- TL_VS_project/TL_flask/views.py
+++ TL_VS_project/TL_flask/views.py
@@ -1,7 +1,15 @@
-from flask import Flask
 from TL_flask import app
+from datetime import datetime
+from flask import render_template
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello Flask!"
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    return render_template(
+        "index.html",
+        title = "Hello Flask",
+        message = "Hello, Flask!",
+        content = " on " + formatted_now)
