5f3d48a7570fb3ed44f2dce48c7831def099b7b9
SergeTopLigin <topligahighlights@gmail.com>
2024-01-17 20:04:35 +0300

    static org

--- TL_VS_project/TL_flask/views.py
+++ TL_VS_project/TL_flask/views.py
@@ -13,3 +13,7 @@ def home():
         title = "Hello Flask",
         message = "Hello, Flask!",
         content = " on " + formatted_now)
+
+@app.route('/api/data')
+def get_data():
+  return app.send_static_file('data.json')
