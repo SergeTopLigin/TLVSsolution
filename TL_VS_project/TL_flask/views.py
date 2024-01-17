d68e9774df5e68296ee403de761a41995ac40b58
SergeTopLigin <topligahighlights@gmail.com>
2024-01-17 21:38:42 +0300

    template layout

--- TL_VS_project/TL_flask/views.py
+++ TL_VS_project/TL_flask/views.py
@@ -17,3 +17,10 @@ def home():
 @app.route('/api/data')
 def get_data():
   return app.send_static_file('data.json')
+
+@app.route('/about')
+def about():
+    return render_template(
+        "about.html",
+        title = "About HelloFlask",
+        content = "Example app page for Flask.")
