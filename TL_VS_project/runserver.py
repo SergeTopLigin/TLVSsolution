0b9fe4bc31e13d4c564bf24a627a472904c58b82
SergeTopLigin <topligahighlights@gmail.com>
2024-01-18 16:34:26 +0300

    upg runserver

--- TL_VS_project/runserver.py
+++ TL_VS_project/runserver.py
@@ -1,7 +1,7 @@
 import os
 from TL_flask import app    # Imports the code from TL-flask/__init__.py
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # запуск локального сервера (на production сервере __name__ != '__main__' и сервер уже запущен)
     HOST = os.environ.get('SERVER_HOST', 'localhost')
 
     try:
@@ -9,4 +9,5 @@ if __name__ == '__main__':
     except ValueError:
         PORT = 5555
 
-    app.run(HOST, PORT)
+    app.run(HOST, PORT, debug=True)     # debug=True для отображения в браузере ошибок при разработке
+                                        # перед размещением на production сервере установить debug=False, чтобы пользователи этого не видели
