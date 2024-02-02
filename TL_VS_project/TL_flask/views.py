c05a9d3b996ef3d8d7e3f35ed25936e7edef2263
SergeTopLigin <topligahighlights@gmail.com>
2024-02-02 22:19:51 +0300

    freeze correct

--- TL_VS_project/TL_flask/views.py
+++ TL_VS_project/TL_flask/views.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask import render_template    # функция для вызова шаблонов по адресу \templates
 
 @app.route('/')
-@app.route('/home/')
+# @app.route('/home/')
 def home():
     now = datetime.now()
     formatted_now = now.strftime("%A, %d %B, %Y at %X")
