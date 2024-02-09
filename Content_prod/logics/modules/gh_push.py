57940c290cd5a56c42ca96ebf594d0f6695e4fb1
SergeTopLigin <topligahighlights@gmail.com>
2024-02-10 00:15:33 +0300

    test

--- Content_prod/logics/modules/gh_push.py
+++ Content_prod/logics/modules/gh_push.py
@@ -22,23 +22,22 @@ def gh_push(main_mod, file_dir, file_name, file_content):
             BugDate = str(datetime.datetime.utcnow())[:19].replace(":", "-")    # текущая дата по UTC, отформатированная под строку
             file_name = BugDate+str(main_mod)+".txt"
 
+        print(path+file_name)
+
         # настройка выгрузки в репо
         from github import Github
         from github import Auth     # Authentication is defined via github.Auth
         import os
         token = os.getenv('GH_token')   # обращение к переменной среды (секрету), установленной в .yml 
         repo_name = os.getenv('repository')
+        auth = Auth.Token(token)    # using an access token
+        g = Github(auth=auth)   # Public Web Github
+        repo = g.get_repo(repo_name)
 
-        print(repo_name)
+        # отправка файла в репо
+        repo.create_file(path+file_name, "add "+file_name, file_content, branch="master")
 
-        # auth = Auth.Token(token)    # using an access token
-        # g = Github(auth=auth)   # Public Web Github
-        # repo = g.get_repo(repo_name)
-
-        # # отправка файла в репо
-        # repo.create_file(path+file_name, "add "+file_name, file_content, branch="master")
-
-        # g.close()
+        g.close()
 
     except:
 
