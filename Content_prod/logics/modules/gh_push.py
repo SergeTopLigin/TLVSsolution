a4a9438ac95c8d2ae2031b1002fe521ed90f2084
SergeTopLigin <topligahighlights@gmail.com>
2024-02-09 23:51:52 +0300

    test

--- Content_prod/logics/modules/gh_push.py
+++ Content_prod/logics/modules/gh_push.py
@@ -20,24 +20,24 @@ def gh_push(main_mod, file_dir, file_name, file_content):
         if file_name == 'bug_file':
             import datetime     # модуль для определния текущей даты для формирования имени bug_file
             BugDate = str(datetime.datetime.utcnow())[:19].replace(":", "-")    # текущая дата по UTC, отформатированная под строку
-            file_name = BugDate+"_"+str(main_mod)+".txt"
+            file_name = BugDate+str(main_mod)+".txt"
 
-        print(file_name)
+        q=1/0
 
-    #     # настройка выгрузки в репо
-    #     from github import Github
-    #     from github import Auth     # Authentication is defined via github.Auth
-    #     import os
-    #     token = os.getenv('GH_token')   # обращение к переменной среды (секрету), установленной в .yml 
-    #     repo_name = os.getenv('repository')
-    #     auth = Auth.Token(token)    # using an access token
-    #     g = Github(auth=auth)   # Public Web Github
-    #     repo = g.get_repo(repo_name)
+        # # настройка выгрузки в репо
+        # from github import Github
+        # from github import Auth     # Authentication is defined via github.Auth
+        # import os
+        # token = os.getenv('GH_token')   # обращение к переменной среды (секрету), установленной в .yml 
+        # repo_name = os.getenv('repository')
+        # auth = Auth.Token(token)    # using an access token
+        # g = Github(auth=auth)   # Public Web Github
+        # repo = g.get_repo(repo_name)
 
-    #     # отправка файла в репо
-    #     repo.create_file(path+file_name, "add "+file_name, file_content, branch="master")
+        # # отправка файла в репо
+        # repo.create_file(path+file_name, "add "+file_name, file_content, branch="master")
 
-    #     g.close()
+        # g.close()
 
     except:
 
