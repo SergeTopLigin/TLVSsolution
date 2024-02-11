ccbfd3880c56bc439421514948a22908dfe6aaa9
SergeTopLigin <topligahighlights@gmail.com>
2024-02-11 15:17:38 +0300

    test

--- Content_prod/logics/TEST.py
+++ Content_prod/logics/TEST.py
@@ -3,4 +3,4 @@ content = 'test'
 import os
 mod_name = os.path.basename(__file__)[:-3]
 from modules.gh_push import gh_push
-gh_push(str(mod_name), 'content_commits', 'standings', content)
+gh_push(str(mod_name), 'sub_results', 'standings.txt', content)
