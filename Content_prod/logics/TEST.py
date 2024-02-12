6758c36880f7cd4704df670a90c42277c6761209
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2024-02-12 22:12:05 +0300

    Update TEST.py

--- Content_prod/logics/TEST.py
+++ Content_prod/logics/TEST.py
@@ -3,4 +3,4 @@ content = 'changed'
 import os
 mod_name = os.path.basename(__file__)[:-3]
 from modules.gh_push import gh_push
-gh_push(str(mod_name), 'sub_results', 'standings.txt', content)
+gh_push(str(mod_name), 'answers', 'standings.txt', content)
