3f721dbcabcce447d3d41f74d5be7db21acc859d
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2024-03-10 20:05:45 +0300

    Update tournament2.py

--- Content_prod/logics/tournament2.py
+++ Content_prod/logics/tournament2.py
@@ -259,7 +259,12 @@ try:    # обработка исключений для определения 
                     Del_tourn.append(tourn)
         for tourn in Del_tourn:
             Ass_TournRateQuot[ass_n].remove(tourn)    
-
+    
+    # тест с выгрузкой результата на GH
+    mod_name = os.path.basename(__file__)[:-3]
+    from modules.gh_push import gh_push
+    gh_push(str(mod_name), 'sub_results', 'PREtournaments.json', Ass_TournRateQuot)
+    
     # приведение всех списков нац турниров Ass_TournRateQuot к виду [Tournament,Season,Rating,Quota,TournID,TournType]
     for ass_n in Ass_TournRateQuot:
         Del_tourn = []  # список турниров на удаление
@@ -288,13 +293,13 @@ try:    # обработка исключений для определения 
                             Del_tourn.append(tourn)     # удалить кубок из списка учитываемых турниров
                     # если есть файл "curr" (появляется в каталоге через 400 дней после 1-го матча "prev"), но не наступила дата его 1-го матча - 
                     # удалить кубок "curr"
-                    if tourn_file.find(tourn[0]) != -1 and tourn_file.find(tourn[1]) != -1 and tourn[1] == "curr":
-                        with open((os.path.abspath(__file__))[:-22]+'/cache/answers/fixtures/'+tourn_file, 'r') as f:
-                            file_content = f.read()
-                        if DateNow <= CupFirst(file_content):
-                            Del_tourn.append(tourn)     # удалить кубок из списка учитываемых турниров
-                    if tourn_file.find(tourn[0]) != -1 and tourn_file.find(tourn[1]) != -1:
-                        tourn[1] = tourn_file[-15:-10]   # изменение "curr/prev" на сезон
+                 #   if tourn_file.find(tourn[0]) != -1 and tourn_file.find(tourn[1]) != -1 and tourn[1] == "curr":
+                  #      with open((os.path.abspath(__file__))[:-22]+'/cache/answers/fixtures/'+tourn_file, 'r') as f:
+                   #         file_content = f.read()
+                    #    if DateNow <= CupFirst(file_content):
+                  #          Del_tourn.append(tourn)     # удалить кубок из списка учитываемых турниров
+                 #   if tourn_file.find(tourn[0]) != -1 and tourn_file.find(tourn[1]) != -1:
+                  #      tourn[1] = tourn_file[-15:-10]   # изменение "curr/prev" на сезон
         for tourn in Del_tourn:     # удаление турниров prev после потери их актуальности
             Ass_TournRateQuot[ass_n].remove(tourn)    
        
