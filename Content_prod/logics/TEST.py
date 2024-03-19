589289b900d1dce76657de394df127c1d7f2779f
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2024-03-20 00:00:43 +0300

    Update TEST.py

--- Content_prod/logics/TEST.py
+++ Content_prod/logics/TEST.py
@@ -43,6 +43,13 @@ if len(random_list) > 0:
     for club in random_list:
         if club in participants:
             slots += 1
+    participants -= random_list
+    # fills = 0    # счётчик заполнений рандомом
+    # import random
+    # while fills < slots:
+        # fill_club = random.choice(random_list)
+        # participants.append(fill_club)
+        # random_list.remove(fill_club)
 
 print(participants)
 print(random_list)
