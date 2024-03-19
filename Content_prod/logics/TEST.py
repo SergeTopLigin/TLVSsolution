b457d55248df8a790b32386c8f0fb3ac69a9cc79
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2024-03-19 13:07:01 +0300

    Update TEST.py

--- Content_prod/logics/TEST.py
+++ Content_prod/logics/TEST.py
@@ -3,3 +3,11 @@ import json
 with open((os.path.abspath(__file__))[:-15]+'/cache/sub_results/final_standings.json', 'r') as j:
     standings = json.load(j)
 
+# same_ranks = [standings[club]['TL_rank'] for club in standings if standings[club]['TL_rank'] == ]
+all_ranks = []
+same_ranks = []
+for club in standings:
+    all_ranks.append(standings[club]['TL_rank'])
+for rank in range(0, len(all_ranks)-1):
+    if all_ranks[rank] == all_ranks[rank+1]:
+        same_ranks.append(all_ranks[rank])
