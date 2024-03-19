ce80cec3c29ddda1ec98c1eae74d6078b73c73c5
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2024-03-19 13:47:41 +0300

    Update TEST.py

--- Content_prod/logics/TEST.py
+++ Content_prod/logics/TEST.py
@@ -8,6 +8,6 @@ all_ranks = []
 same_ranks = []
 for club in standings:
     all_ranks.append(standings[club]['TL_rank'])
-for rank in range(0, len(all_ranks)-1):
-    if all_ranks[rank] == all_ranks[rank+1]:
+for rank in range(0, len(all_ranks)-2):
+    if all_ranks[rank] == all_ranks[rank+1] and all_ranks[rank] not in same_ranks:
         same_ranks.append(all_ranks[rank])
