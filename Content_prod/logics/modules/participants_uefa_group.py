dab47dc25e95b3655fccf400606f0288c8f0849c
SergeTopLigin <topligahighlights@gmail.com>
2024-03-21 00:34:21 +0300

    participants criteria

--- Content_prod/logics/modules/participants_uefa_group.py
+++ Content_prod/logics/modules/participants_uefa_group.py
@@ -21,21 +21,47 @@ def participants_uefa_group(tourn, tourn_id, season, quota, prev):
         participants = []   # результирующий список участников от турнира
         import os
         import json
+        import random
         with open((os.path.abspath(__file__))[:-42]+'/cache/sub_results/final_standings.json', 'r') as j:
-            standings = json.load(j)
+            TL_standings = json.load(j)
 
         # актуализация fixtures и standings турнира
         from modules.uefa_tourn_files import uefa_tourn_files
         uefa_tourn_files(tourn, season, tourn_id, 'group')
         
         file_find = 0   # флаг наличия файла турнира
-        for tourn_file in os.listdir((os.path.abspath(__file__))[:-42]+'/cache/answers/fixtures'):
+        for tourn_file in os.listdir((os.path.abspath(__file__))[:-42]+'/cache/answers/standings'):
             if tourn in tourn_file and season in tourn_file:
                 file_find = 1
+                with open((os.path.abspath(__file__))[:-42]+'/cache/sub_results/'+tourn, 'r') as j:
+                    tourn_standings = json.load(j)
+                break
+        
+        if file_find == 1:
+            # список с критериями определения лучших best_define = [{'club': , 'id': , 'pts/pl': , 'dif/pl': , 'TL_rank': , 'random_rank': }]
+            best_define = []
+            for group in tourn_standings['response'][0]['league']['standings']:
+                for club in group:
+                    club_name = club['team']['name']
+                    club_id = club['team']['id']
+                    pts_pl = round(club['points'] / club['all']['played'], 2)
+                    dif_pl = round(club['goalsDiff'] / club['all']['played'], 2)
+                    if club['team']['name'] in TL_standings:
+                        TL_rank = [TL_standings[TL_club]['TL_rank'] for TL_club in TL_standings if TL_club == club['team']['name']][0]
+                    else:
+                        TL_rank = -5
+                    random_rank = random.random()
+                    best_define.append({'club': club_name, 'id': club_id, 'pts/pl': pts_pl, 'dif/pl': dif_pl, 'TL_rank': TL_rank, 'random_rank': random_rank})
+            best_define.sort(key=lambda crit: (crit['pts/pl'], crit['dif/pl'], crit['TL_rank'], crit['random_rank']), reverse=True)
+            number = 0
+            for club in best_define:
+                if number < quota and club['club'] not in [prev_club['club'] for prev_club in prev]:
+                    number += 1
+                    participants.append({'club': club['club'], 'id': club['id']})
 
+        
 
-
-        if file_find == 0:    # если файла нет - определить по TL standings (3 критерий) по /club_sets
+        if file_find == 0:    # если файла нет - определить по TL standings (3 критерий) по /club_sets и random (4 критерий)
             set_season = '20'+season[:2]+'-20'+season[3:]    # YYYY-YYYY
             LeagueClubSetID = []    # создание списка id из файла UefaTournamentClubSet
             dir_sets = os.listdir((os.path.abspath(__file__))[:-42]+'/cache/sub_results/club_sets')
@@ -46,14 +72,15 @@ def participants_uefa_group(tourn, tourn_id, season, quota, prev):
                     end_substr = line.find('.',kursor)    # определение конца искомой подстроки (поиск символа "." после позиции курсора)
                     LeagueClubSetID.append(int(line[kursor:end_substr]))
             number = 0
-            for club in standings:
-                if standings[club]['IDapi'] in LeagueClubSetID and number < quota and club not in [prev_club['club'] for prev_club in prev]:
+            for club in TL_standings:
+                if TL_standings[club]['IDapi'] in LeagueClubSetID and number < quota and club not in [prev_club['club'] for prev_club in prev]:
                     number += 1
-                    participants.append({'club': club, 'id': standings[club]['IDapi']})
+                    participants.append({'club': club, 'id': TL_standings[club]['IDapi']})
             # учет 4-го критерия (рандом) при прочих равных
             last_participant = participants[-1]['club']
             # список для рандома: из клубов, TL rank которых = TL rank последнего по квоте участника
-            random_list = [{'club': club, 'id': standings[club]['IDapi']} for club in standings if standings[club]['TL_rank'] == standings[last_participant]['TL_rank']]
+            random_list = [{'club': club, 'id': TL_standings[club]['IDapi']} for club in TL_standings if \
+            TL_standings[club]['TL_rank'] == TL_standings[last_participant]['TL_rank']]
             if len(random_list) > 0:
                 slots = 0   # инициализация количества слотов в списке участников, занятых клубами с одинаковыми TL rank
                 for club in random_list:
@@ -61,14 +88,13 @@ def participants_uefa_group(tourn, tourn_id, season, quota, prev):
                         slots += 1
                         participants.remove(club)
                 fills = 0    # счётчик заполнений слотов рандомом
-                import random
                 while fills < slots:
                     fills += 1
                     fill_club = random.choice(random_list)
                     participants.append(fill_club)
                     random_list.remove(fill_club)
 
-
+        return(participants)
 
     except: 
         # запись ошибки/исключения в переменную через временный файл
