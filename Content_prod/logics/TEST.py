ef48e8a772c2ddac7852d34876e8d1ce034612c3
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2024-03-21 10:24:18 +0300

    Update TEST.py

--- Content_prod/logics/TEST.py
+++ Content_prod/logics/TEST.py
@@ -1,7 +1,7 @@
 import os
 import json
 with open((os.path.abspath(__file__))[:-15]+'/cache/sub_results/final_standings.json', 'r') as j:
-    standings = json.load(j)
+    TL_standings = json.load(j)
 
 # same_ranks = [standings[club]['TL_rank'] for club in standings if standings[club]['TL_rank'] == ]
 # all_ranks = []
@@ -29,6 +29,24 @@ with open((os.path.abspath(__file__))[:-15]+'/cache/sub_results/club_sets/'+set_
         kursor = line.find('id:',0) +3    # переместить курсор перед искомой подстрокой
         end_substr = line.find('.',kursor)    # определение конца искомой подстроки (поиск символа "." после позиции курсора)
         LeagueClubSetID.append(int(line[kursor:end_substr]))
+
+for clubID in LeagueClubSetID:
+    club_name = [TL_club for TL_club in TL_standings if clubID == TL_standings[TL_club]['IDapi']][0]
+                    club_id = club['team']['id']
+                    pts_pl = round(club['points'] / club['all']['played'], 2)
+                    dif_pl = round(club['goalsDiff'] / club['all']['played'], 2)
+                    if club['team']['name'] in TL_standings:
+                        TL_rank = [TL_standings[TL_club]['TL_rank'] for TL_club in TL_standings if TL_club == club['team']['name']][0]
+                    else:
+                        TL_rank = -5
+                    random_rank = random.random()
+                    best_define.append({'club': club_name, 'id': club_id, 'pts/pl': pts_pl, 'dif/pl': dif_pl, 'TL_rank': TL_rank, 'random_rank': random_rank})
+            best_define.sort(key=lambda crit: (crit['pts/pl'], crit['dif/pl'], crit['TL_rank'], crit['random_rank']), reverse=True)
+            number = 0
+            for club in best_define:
+                if number < quota and club['club'] not in [prev_club['club'] for prev_club in prev]:
+                    number += 1
+                    participants.append({'club': club['club'], 'id': club['id']})
 
 number = 0
 for club in standings:
