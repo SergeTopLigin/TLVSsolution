329bae6103340c12a1bc49c879dab4c84611de85
SergeTopLigin <topligahighlights@gmail.com>
2024-03-26 19:13:25 +0300

    participants uefa playoff

--- Content_prod/logics/modules/participants_uefa_playoff.py
+++ Content_prod/logics/modules/participants_uefa_playoff.py
@@ -44,19 +44,59 @@ def participants_uefa_playoff(tourn, tourn_id, season, quota):
                     tourn_fixtures = json.load(j)
                 break
         
-        # словарь стадий
-        rounds = {}    # {round: {'last_date': , 'status': }}
-        for fixture in tourn_fixtures['response']:
-            if fixture['league']['round'] not in rounds or\
-            fixture['fixture']['timestamp'] > [rounds[stage]['last_date'] for stage in rounds if fixture['league']['round'] == stage][0]:
-                rounds[fixture['league']['round']] = {'last_date': fixture['fixture']['timestamp'], 'status': fixture['fixture']['status']['short']}
-        # определение текущей стадии
-        cancelled = ['FT', 'AET', 'PEN', 'CANC', 'AWD', 'WO']   # список статусов завершения
-        rounds_cont = [{stage: rounds[stage]['last_date']} for stage in rounds if rounds[stage]['status'] not in cancelled]   # незавершенные стадии [{round: last_date}, ]
-        curr_round = min(rounds_cont, key = rounds_cont.get)    # текущая стадия - ранняя из незавершенных
-        # выборка стадий только playoff и не позднее текущей
-        group_rounds = [{stage: rounds[stage]['last_date']} for stage in rounds if 'Group' in stage]   # все групповые rounds
-        last_group_round_time = max(group_rounds.values())    # время последнего группового round
+        if file_find == 1:
+            # словарь стадий
+            rounds = {}    # {round: {'last_date': , 'status': }}
+            for fixture in tourn_fixtures['response']:
+                if fixture['league']['round'] not in rounds or\
+                fixture['fixture']['timestamp'] > [rounds[stage]['last_date'] for stage in rounds if fixture['league']['round'] == stage][0]:
+                    rounds[fixture['league']['round']] = {'last_date': fixture['fixture']['timestamp'], 'status': fixture['fixture']['status']['short']}
+            # определение времени текущей стадии
+            cancelled = ['FT', 'AET', 'PEN', 'CANC', 'AWD', 'WO']   # список статусов завершения
+            rounds_cont_time = {stage: rounds[stage]['last_date'] for stage in rounds if rounds[stage]['status'] not in cancelled}   # время незавершенных стадий
+            curr_round_time = min(rounds_cont_time.values())    # время текущей стадии - ранней из незавершенных
+            # определение времени последнего группового round
+            group_rounds = {stage: rounds[stage]['last_date'] for stage in rounds if 'Group' in stage}   # все групповые rounds
+            last_group_round_time = max(group_rounds.values())    # время последнего группового round
+            # выборка стадий только playoff и не позднее текущей
+            playoff_rounds = {stage: rounds[stage]['last_date'] for stage in rounds if rounds[stage]['last_date'] > last_group_round_time and\
+             rounds[stage]['last_date'] <= curr_round_time}
+            # выборка стадий от групповых матчей до текущей стадии
+            main_rounds = list(group_rounds.keys()) + list(playoff_rounds.keys())
+            # сортировка стадий плейофф от текущей в прошлое
+            playoff_rounds = sorted(playoff_rounds, key=lambda stage: playoff_rounds[stage], reverse=True)
+            
+            reg_time = ['ET', 'BT', 'P', 'FT', 'AET', 'PEN']  # список статусов окончания основного времени
+            for stage in playoff_rounds:
+                # набор stage_set [{'club': , 'id': ,'pts': , 'dif': , 'pl': , 'pts/pl': , 'dif/pl': , 'TL_rank': , 'random_rank': }]
+                stage_set = []
+                for fixture in tourn_fixtures['response']:
+                    if fixture['league']['round'] == stage and fixture['teams']['home']['id'] not in [stage['id'] for stage in stage_set]\
+                    and fixture['teams']['home']['id'] not in [club['id'] for club in participants]:
+                        stage_set.append({'club': fixture['teams']['home']['name'], 'id': fixture['teams']['home']['id'],\
+                         'pts': 0, 'dif': 0, 'pl': 0, 'pts/pl': None, 'dif/pl': None, 'TL_rank': None, 'random_rank': None})
+                    if fixture['league']['round'] == stage and fixture['teams']['away']['id'] not in [stage['id'] for stage in stage_set]\
+                    and fixture['teams']['away']['id'] not in [club['id'] for club in participants]:
+                        stage_set.append({'club': fixture['teams']['away']['name'], 'id': fixture['teams']['away']['id'],\
+                         'pts': 0, 'dif': 0, 'pl': 0, 'pts/pl': None, 'dif/pl': None, 'TL_rank': None, 'random_rank': None})
+                # сортировка stage_set
+                for club in stage_set:
+                    for fixture in tourn_fixtures['response']:
+                        if fixture['league']['round'] in main_rounds and club['id'] == fixture['teams']['home']['id']\
+                         and fixture['fixture']['status']['short'] in reg_time:
+                            club['pl'] += 1
+                            if fixture['score']['fulltime']['home'] > fixture['score']['fulltime']['away']:     club['pts'] += 3
+                            elif fixture['score']['fulltime']['home'] == fixture['score']['fulltime']['away']:  club['pts'] += 1
+                            club['dif'] += fixture['score']['fulltime']['home'] - fixture['score']['fulltime']['away']
+                        if fixture['league']['round'] in main_rounds and club['id'] == fixture['teams']['away']['id']\
+                         and fixture['fixture']['status']['short'] in reg_time:
+                            club['pl'] += 1
+                            if fixture['score']['fulltime']['home'] < fixture['score']['fulltime']['away']:     club['pts'] += 3
+                            elif fixture['score']['fulltime']['home'] == fixture['score']['fulltime']['away']:  club['pts'] += 1
+                            club['dif'] += fixture['score']['fulltime']['away'] - fixture['score']['fulltime']['home']
+
+
+                if len(stage_set) == quota:
 
         
 
