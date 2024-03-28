5ef8a4550d4bf8d5004da971cd41953007144fe6
SergeTopLigin <topligahighlights@gmail.com>
2024-03-28 23:26:50 +0300

    participants uefa playoff

--- Content_prod/logics/modules/participants_uefa_playoff.py
+++ Content_prod/logics/modules/participants_uefa_playoff.py
@@ -57,7 +57,10 @@ def participants_uefa_playoff(tourn, tourn_id, season, quota):
             # определение времени текущей стадии
             cancelled = ['FT', 'AET', 'PEN', 'CANC', 'AWD', 'WO']   # список статусов завершения
             rounds_cont_time = {stage: rounds[stage]['last_date'] for stage in rounds if rounds[stage]['status'] not in cancelled}   # время незавершенных стадий
-            curr_round_time = min(rounds_cont_time.values())    # время текущей стадии - ранней из незавершенных
+            if len(rounds_cont_time) != 0:   # если есть хотя бы одна неавершенная стадия
+                curr_round_time = min(rounds_cont_time.values())    # время текущей стадии - ранней из незавершенных
+            else:
+                curr_round_time = 4000000000      # символическое время след стадии, если еще не было жеребьевки и незавершенных стадий нет
             # определение времени последнего группового round
             group_rounds = {stage: rounds[stage]['last_date'] for stage in rounds if 'Group' in stage}   # все групповые rounds
             last_group_round_time = max(group_rounds.values())    # время последнего группового round
@@ -68,8 +71,12 @@ def participants_uefa_playoff(tourn, tourn_id, season, quota):
             main_rounds = list(group_rounds.keys()) + list(playoff_rounds.keys())
             # сортировка стадий плейофф от текущей в прошлое
             playoff_rounds = sorted(playoff_rounds, key=lambda stage: playoff_rounds[stage], reverse=True)
-            
-            # формирование standings турниров из которых возможны переходы клубов с 3-х мест групп
+            # добавить в начало списка стадий 'next round', если еще не было ее жеребьевки и незавершенных стадий нет
+            if curr_round_time == 4000000000 and playoff_rounds[0] != 'Final':  
+                playoff_rounds.insert(0, 'next round')
+
+            # для сезонов 23-24 и ранее
+            # формирование standings турниров из которых возможны переходы клубов с 3-х мест групп и
             if int(season[:2]) < 24:
                 if 'UEL' in tourn:
                     if 'UCL '+season+' stan.json' not in os.listdir((os.path.abspath(__file__))[:-44]+'/cache/answers/standings'):
@@ -85,21 +92,55 @@ def participants_uefa_playoff(tourn, tourn_id, season, quota):
                         runner_push(str(mod_name), 'standings', 'UEL '+season+' stan.json', UELstan)
                     with open((os.path.abspath(__file__))[:-44]+'/cache/answers/standings/UEL '+season+' stan.json', 'r') as j:
                         drop_tourn_standings = json.load(j)
+            # standings UEL, UECL для учета 1-х мест групп в 1/16 (тк они начинают плейофф с 1/8), 
+            # standings турнира, если квота > количества участников 1-й стадии плейофф
+            if tourn+' '+season+' stan.json' not in os.listdir((os.path.abspath(__file__))[:-44]+'/cache/answers/standings'):
+                tourn_stan = api_key("/standings?league="+tourn_id+"&season=20"+season[:2])
+                gh_push(str(mod_name), 'standings', tourn+' '+season+' stan.json', tourn_stan)
+                runner_push(str(mod_name), 'standings', tourn+' '+season+' stan.json', tourn_stan)
+            with open((os.path.abspath(__file__))[:-44]+'/cache/answers/standings/'+tourn+' '+season+' stan.json', 'r') as j:
+                tourn_standings = json.load(j)
+            
             
             # набор и сортировка клубов стадий для заполнения квоты турнира
             reg_time = ['ET', 'BT', 'P', 'FT', 'AET', 'PEN']  # список статусов окончания основного времени
             for stage in playoff_rounds:
+                
                 # набор stage_set [{'club': , 'id': ,'pts': , 'dif': , 'pl': , 'pts/pl': , 'dif/pl': , 'TL_rank': , 'random_rank': }]
                 stage_set = []
-                for fixture in tourn_fixtures['response']:
-                    if fixture['league']['round'] == stage and fixture['teams']['home']['id'] not in [club['id'] for club in stage_set]\
-                    and fixture['teams']['home']['id'] not in [club['id'] for club in participants]:
-                        stage_set.append({'club': fixture['teams']['home']['name'], 'id': fixture['teams']['home']['id'],\
-                         'pts': 0, 'dif': 0, 'pl': 0, 'pts/pl': None, 'dif/pl': None, 'TL_rank': None, 'random_rank': None})
-                    if fixture['league']['round'] == stage and fixture['teams']['away']['id'] not in [club['id'] for club in stage_set]\
-                    and fixture['teams']['away']['id'] not in [club['id'] for club in participants]:
-                        stage_set.append({'club': fixture['teams']['away']['name'], 'id': fixture['teams']['away']['id'],\
-                         'pts': 0, 'dif': 0, 'pl': 0, 'pts/pl': None, 'dif/pl': None, 'TL_rank': None, 'random_rank': None})
+                if stage == 'next round':   # если известны все участники текущей стадии, но не было ее жеребьевки (отсутствует в fixtures)
+                    # набрать stage_set из победителей предыдущей стадии playoff_rounds[1]
+                    next_stage_set = []
+                    # набрать список участников стадии [{'club': , 'id': , 'goalsDiff': (goals+penalty)}, ...]
+                    for fixture in tourn_fixtures['response']:
+                        if fixture['league']['round'] == playoff_rounds[1] and fixture['teams']['home']['id'] not in [club['id'] for club in next_stage_set]:
+                            next_stage_set.append({'club': fixture['teams']['home']['name'], 'id': fixture['teams']['home']['id'], 'goalsDiff': 0})
+                        if fixture['league']['round'] == playoff_rounds[1] and fixture['teams']['away']['id'] not in [club['id'] for club in next_stage_set]:
+                            next_stage_set.append({'club': fixture['teams']['away']['name'], 'id': fixture['teams']['away']['id'], 'goalsDiff': 0})
+                    for fixture in tourn_fixtures['response']:
+                        pen_home = fixture['score']['penalty']['home'] if type(fixture['score']['penalty']['home']) == int else 0
+                        pen_away = fixture['score']['penalty']['away'] if type(fixture['score']['penalty']['away']) == int else 0
+                        for club in next_stage_set:
+                            if fixture['league']['round'] == playoff_rounds[1] and fixture['teams']['home']['id'] == club['id']:
+                                club['goalsDiff'] += fixture['goals']['home'] + pen_home - fixture['goals']['away'] - pen_away
+                            if fixture['league']['round'] == playoff_rounds[1] and fixture['teams']['away']['id'] == club['id']:
+                                club['goalsDiff'] += fixture['goals']['away'] + pen_away - fixture['goals']['home'] - pen_home
+                    # из списка участников стадии набрать список прошедших: goalsDiff > 0
+                    for club in next_stage_set:
+                        if club['goalsDiff'] > 0:
+                            stage_set.append({'club': club['club'], 'id': club['id'],\
+                             'pts': 0, 'dif': 0, 'pl': 0, 'pts/pl': None, 'dif/pl': None, 'TL_rank': None, 'random_rank': None})
+                else:       # если текущая стадия есть в fixtures
+                    for fixture in tourn_fixtures['response']:
+                        if fixture['league']['round'] == stage and fixture['teams']['home']['id'] not in [club['id'] for club in stage_set]\
+                        and fixture['teams']['home']['id'] not in [club['id'] for club in participants]:
+                            stage_set.append({'club': fixture['teams']['home']['name'], 'id': fixture['teams']['home']['id'],\
+                             'pts': 0, 'dif': 0, 'pl': 0, 'pts/pl': None, 'dif/pl': None, 'TL_rank': None, 'random_rank': None})
+                        if fixture['league']['round'] == stage and fixture['teams']['away']['id'] not in [club['id'] for club in stage_set]\
+                        and fixture['teams']['away']['id'] not in [club['id'] for club in participants]:
+                            stage_set.append({'club': fixture['teams']['away']['name'], 'id': fixture['teams']['away']['id'],\
+                             'pts': 0, 'dif': 0, 'pl': 0, 'pts/pl': None, 'dif/pl': None, 'TL_rank': None, 'random_rank': None})
+
                 # набор критериев и сортировка stage_set
                 for club in stage_set:
                     for fixture in tourn_fixtures['response']:
@@ -116,7 +157,7 @@ def participants_uefa_playoff(tourn, tourn_id, season, quota):
                             elif fixture['score']['fulltime']['home'] == fixture['score']['fulltime']['away']:  club['pts'] += 1
                             club['dif'] += fixture['score']['fulltime']['away'] - fixture['score']['fulltime']['home']
                     # для сезона 23/24 и ранее: набор очков и разниц из групп других турниров УЕФА (для перешедших с 3-х мест) с кор коэффициентами 
-                    if int(season[:2]) < 24:
+                    if int(season[:2]) < 24 and ('UEL' in tourn or 'UECL' in tourn):
                         for group in drop_tourn_standings['response'][0]['league']['standings']:
                             for rank in group:
                                 if rank['team']['id'] == club['id']:
@@ -132,10 +173,15 @@ def participants_uefa_playoff(tourn, tourn_id, season, quota):
                         club['TL_rank'] = -5
                     club['random_rank'] = random.random()
                 stage_set.sort(key=lambda crit: (crit['pts/pl'], crit['dif/pl'], crit['TL_rank'], crit['random_rank']), reverse=True)
-
-                if len(stage_set) == quota:
-                    participants = [{'club': club['club'], 'id': club['id']} for club in stage_set]
+                
+                # набор квоты турнира
+                club_from_stage = 0
+                while len(participants) < quota and club_from_stage < len(stage_set):
+                    participants.append({'club': stage_set[club_from_stage]['club'], 'id': stage_set[club_from_stage]['id']})
+                    club_from_stage += 1
+                if quota == len(participants):
                     break
+                # если квота больше количества участников плейофф: добирать из групп
 
         
 
