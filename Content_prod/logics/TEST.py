cc3f4b196346e20cf512d8db4f221c7adfff93da
SergeTopLigin <topligahighlights@gmail.com>
2024-04-16 19:05:37 +0300

    parts test 2

--- Content_prod/logics/TEST.py
+++ Content_prod/logics/TEST.py
@@ -5,9 +5,11 @@ import os, datetime, time, json
 # print(time.time() - os.path.getmtime((os.path.abspath(__file__))[:-15]+'/cache/answers/standings/NOR League 23-24 stan.json'))
 with open((os.path.abspath(__file__))[:-15]+'/cache/answers/standings/BEL League 23-24 stan.json', 'r', encoding='utf-8') as j:
     league_standings = json.load(j)
-update_time = league_standings['response'][0]['league']['standings'][0][0]['update'][:10]
+for group in league_standings['response'][0]['league']['standings']:
+    for club in group:
+        update_time = club['update'][:10]
 # print(datetime.datetime.utcnow() + datetime.timedelta(days=7))
-print(update_time)
+        print(update_time)
 # print(datetime.datetime.utcnow() + datetime.timedelta(days=7) < datetime.datetime(int(update_time[:4]), int(update_time[5:7]), int(update_time[8:])))
 
 # print(datetime.datetime.utcnow().year if datetime.datetime.utcnow().month > 4 else datetime.datetime.utcnow().year -1)
