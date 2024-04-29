15f775873a85d6126708af5a47884063f3d38f7f
SergeTopLigin <topligahighlights@gmail.com>
2024-04-29 23:46:50 +0300

    worktime str datetime

--- Content_prod/logics/TEST.py
+++ Content_prod/logics/TEST.py
@@ -1,18 +1,3 @@
-# import os, json
-# with open((os.path.abspath(__file__))[:-15]+'/cache/sub_results/participants_nat.json', 'r', encoding='utf-8') as j:
-#     participants = json.load(j)
-
-# from modules.nat_tournaments import Nat_Tournaments
-# nat_tournaments = Nat_Tournaments()
-# tourns = []
-# for ass in participants:
-#     if len(participants[ass])>1:
-#         for nat_ass in nat_tournaments:
-#             if nat_ass == ass:
-#                 for tourn in nat_tournaments[nat_ass]:
-#                     if [tourn[0], tourn[3]] not in tourns and tourn[3] != -1:
-#                         tourns.append([tourn[0], tourn[3]])
-# print(tourns)
-
-if 'Cup' in 'ENG LCup':
-    print('yes')
+import datetime
+curr_datetime = datetime.datetime.utcnow()
+print(str(curr_datetime))
--- Content_prod/logics/games.py
+++ Content_prod/logics/games.py
@@ -185,7 +185,7 @@ try:    # обработка исключений для определения 
                     games[match['teams']['home']['id']].append(add_game(match, match['teams']['home']['id'], tourn[0], season))
                     games[match['teams']['away']['id']].append(add_game(match, match['teams']['away']['id'], tourn[0], season))
     # фиксация в worktimes.json времени текущего расчета
-    worktimes.append([curr_datetime, curr_timestamp])
+    worktimes.append([str(curr_datetime), curr_timestamp])
     # и выгрузка worktimes.json в репо и на runner: /sub_results
     gh_push(str(mod_name), 'sub_results', 'worktimes.json', worktimes)
     runner_push(str(mod_name), 'sub_results', 'worktimes.json', worktimes)
